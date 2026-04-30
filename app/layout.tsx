ff34710daa4bfee2c27e174dada48cdacf37d8c6
Othavio Quiliao <othavioaqa@gmail.com>
2026-04-30 11:56:09 -0300

    feat: primeiro commit

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import { Geist, Geist_Mono, IBM_Plex_Sans } from "next/font/google"
+
+import { ThemeProvider } from "@/components/theme-provider"
+import { TooltipProvider } from "@/components/ui/tooltip"
+import { cn } from "@/lib/utils"
+import "./globals.css"
+
+const ibmPlexSansHeading = IBM_Plex_Sans({
+  subsets: ["latin"],
+  variable: "--font-heading",
+})
+
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        geist.variable,
+        ibmPlexSansHeading.variable
+      )}
+    >
+      <body>
+        <ThemeProvider>
+          <TooltipProvider>{children}</TooltipProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
